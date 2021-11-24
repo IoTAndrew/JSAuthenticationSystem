cbb87b992fcad2d007f094a45daa9a23bb11e7dd
andreilegit <al3286957@gmail.com>
2021-11-24 17:52:33 +0200

    Program completed

--- passport-config.js
+++ passport-config.js
@@ -1,11 +1,11 @@
-const LocalStrategy = require ('/passport-local')
-    .strategy
+const LocalStrategy = require ('passport-local')
+    .Strategy
 const bcrypt = require('bcrypt')
 
 
 
-function initialize(passport) {
-    const authenticateUser = (email, password, done) => {
+function initialize(passport, getUserByEmail, getUserById) {
+    const authenticateUser = async (email, password, done) => {
         const user = getUserByEmail(email)
         if (user == null) {
             return done(null, false, {message: 'No such user'})
@@ -18,14 +18,15 @@ function initialize(passport) {
             else {
                 return done(null, false, {message: 'Password incorrect'})
             }
-        } catch {
+        } catch (e){
             return done(e)
         }
     }
 
-    passport.use(new LocalStrategy({usernameField: 'email'}), authenticateUser)
-    passport.serializeUser((user, done) => { })
-    passport.deserializeUser((id, done) => { })
+    passport.use(new LocalStrategy({usernameField: 'email'}, authenticateUser))
+    passport.serializeUser((user, done) => done(null, user.id))
+    passport.deserializeUser((id, done) => { return done(null, getUserById(id))
+    })
 }
 
 module.exports = initialize
