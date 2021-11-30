2c5917bb998a85e416892485a505d0d4415d90e5
andreilegit <al3286957@gmail.com>
2021-11-30 12:40:06 +0200

    Modding to work with SQLite, still

--- passport-config.js
+++ passport-config.js
@@ -1,19 +1,20 @@
 const LocalStrategy = require ('passport-local')
     .Strategy
 const bcrypt = require('bcrypt')
+const knex = require("./db/knex")
 
 
 
-function initialize(passport, getUserByEmail, getUserById) {
+function initialize(passport, getUserById) {
     const authenticateUser = async (email, password, done) => {
-        const user = getUserByEmail(email)
-        if (user == null) {
+        const userData = await knex("userCredentials").where({email: email, password: 1}).select()
+        if (userData == null) {
             return done(null, false, {message: 'No such user'})
         }
 
         try {
-            if (await bcrypt.compare(password, user.password)){
-                return done(null, user)
+            if (await bcrypt.compare(password, userData.password)){
+                return done(null, userData)
             }
             else {
                 return done(null, false, {message: 'Password incorrect'})
